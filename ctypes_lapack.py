33a0b10765f18dad0a87065a9dc036bf65ee528e
mirok <miroslav.kuchta@gmail.com>
2016-04-20 12:52:58 +0200

    saveing 0 progress on ctypes, no longer needed

--- ctypes_lapack.py
+++ ctypes_lapack.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-LAPACK_LIB = '/home/miro3/Documents/Programming/julia/usr/lib/libopenblas64_.so'
+LAPACK_LIB = '/mn/anatu/ansatte-u8/mirok/Documents/Programming/julia/usr/lib/libopenblas64_.so' 
+LAPACK_LIB = '/mn/anatu/ansatte-u8/mirok/.hashdist/bld/openblas/c7qemuijpr3x/lib/libopenblas.so'
 
  
 def s3d_eig(d, u):
@@ -10,19 +11,19 @@ def s3d_eig(d, u):
 
     assert len(d) == len(u)+1
     # The routine overwrites the argument, so lets make a copy
-    D = np.array(d, copy=True, order='F', dtype=float)
-    E = np.array(u, copy=True, order='F', dtype=float)
+    D = np.array(d, copy=True, order='C', dtype=float)
+    E = np.array(u, copy=True, order='C', dtype=float)
     N = len(d)
     
     # Some helpers
-    matrix_layout = 102
+    matrix_layout = 101
     double_p = np.ctypeslib.ndpointer(dtype=np.float64, ndim=1)
     double_pp = np.ctypeslib.ndpointer(dtype=np.float64, ndim=2)
-    int_p = np.ctypeslib.ndpointer(dtype=np.int, ndim=1)
+    int_p = np.ctypeslib.ndpointer(dtype=np.int64, ndim=1)
     
     # Define the function
     lapack = ctypes.CDLL(LAPACK_LIB)
-    foo = lapack.LAPACKE_dstegr64_
+    foo = lapack.LAPACKE_dstegr
 
     # int matrix_layout, char jobz,     char range,     lapack_int n, 
     # double *d,         double *e,     double vl,      double vu, 
@@ -33,7 +34,7 @@ def s3d_eig(d, u):
     foo.argtypes = [c_int,     c_char,    c_char,   c_int,
                     double_p,  double_p,  c_double, c_double,
                     c_int,     c_int,     c_double, int_p,
-                    double_p,  double_p, c_int,    int_p]
+                    double_p,  double_pp, c_int,    int_p]
     
     # matrix_layout, jobz, range, N, D, E, vl, vu, il, iu, abstol, m
     # 
@@ -42,11 +43,11 @@ def s3d_eig(d, u):
     vl, vu = 0., 0.
     il, iu = 0, 0
     abstol = 0.
-    m = np.zeros(N, dtype=np.int)
+    m = np.zeros(1, dtype=np.int64)
     w = np.zeros(N, dtype=np.float64)
-    z = np.zeros(N, dtype=np.float64)
+    z = np.zeros((N, N), dtype=np.float64)
     ldz = N
-    isuppz = np.zeros(2*N, dtype=np.int)
+    isuppz = np.zeros(2*N, dtype=np.int64)
 
     info = foo(matrix_layout, jobz, range, N,
                D,             E,    vl,    vu,
@@ -58,7 +59,7 @@ def s3d_eig(d, u):
 
 # ----------------------------------------------------------------------------
 
-d = np.random.rand(100)
-e = np.random.rand(99)
+d = np.random.rand(10)+1
+e = np.zeros(9)
 
 s3d_eig(d, e)
